43e10b649b5a36b5ee561798444760a5fe945c43
HilalSyah <159630685+HilalSyah@users.noreply.github.com>
2025-02-03 11:36:37 +0700

    Feat:[HS] Add SplashBinding

---
+++ lib/Splash/SplashScreen_Binding.dart
@@ -0,0 +1,9 @@
+import 'package:get/get.dart';
+import 'package:project_firebase_imprv/Profile/profile_controller.dart';
+
+class SplashScreenBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<ProfileControllerFB>(() => ProfileControllerFB());
+  }
+}
